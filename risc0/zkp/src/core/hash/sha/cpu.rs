78e9d954c5dcb60c38983b1e1bacc000ea2b3948
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-08 16:28:25 -0300

    Test commenting more compress fuinction

--- risc0/zkp/src/core/hash/sha/cpu.rs
+++ risc0/zkp/src/core/hash/sha/cpu.rs
@@ -59,12 +59,12 @@ impl Sha256 for Impl {
         for word in state.iter_mut() {
             *word = word.to_be();
         }
-        let mut blocks = u8s.chunks_exact(64);
-        for block in blocks.by_ref() {
-            sha2::compress256(&mut state, slice::from_ref(GenericArray::from_slice(block)));
-        }
+        let blocks = u8s.chunks_exact(64);
+        // for block in blocks.by_ref() {
+        //     sha2::compress256(&mut state, slice::from_ref(GenericArray::from_slice(block)));
+        // }
         let remainder = blocks.remainder();
-        if !remainder.is_empty() {
+        if false {
             let mut last_block: GenericArray<u8, U64> = GenericArray::default();
             bytemuck::cast_slice_mut(last_block.as_mut_slice())[..remainder.len()]
                 .clone_from_slice(remainder);
